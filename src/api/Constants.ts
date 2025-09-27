9ccbd52fec6e85712793f4dc64e52a049ff225b4
Garik Bernecyan <bernecyan.dev@gmail.com>
2025-09-27 23:03:29 +0400

    wp

--- src/api/Constants.ts
+++ src/api/Constants.ts
@@ -3,9 +3,10 @@ export default {
   BASE_URL_DEV: 'https://sandbox.ticketfairy.com/api',
   BASE_URL_STAG: 'https://preview.ttf.fluxtech.me/api',
   // CLIENT_ID: 'e9d8f8922797b4621e562255afe90dbf',
-  CLIENT_ID: '11b907373250ac4813129922b039a828',
+  CLIENT_ID: '79795e7f09f9af5472fd6980f1dbcce2',
   // CLIENT_SECRET: 'b89c191eff22fdcf84ac9bfd88d005355a151ec2c83b26b9',
-  CLIENT_SECRET: '48118a9b8ffd4b120dc63861e6d6c0202fef4a5dbc46b53b',
+  CLIENT_SECRET: '58c27cb6ebcfd33ff9bfcd94e2c62d90ca56ada9dfc7126b',
+
   PRIVACY_POLICY_LINK: 'https://www.ticketfairy.com/privacy-policy/',
   TIMEOUT: 60000,
 }
